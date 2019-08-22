be9a0ef18e47ed5e6ea6e3980c2b5fc03debec23
Tomasz Ducin <tomasz@ducin.it>
2019-08-22 09:41:40 +0200

    controlling todomvc

--- test/todomvc.js
+++ test/todomvc.js
@@ -8,10 +8,20 @@ const URL = 'http://todomvc.com/examples/jquery/';
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
+    slowMo: 100,
   })
 
   const page = await browser.newPage()
-  await page.goto(URL)
+  await page.goto(URL, { waitUntil: 'networkidle2' })
+
+  // gdyby to było dynamiczne
+  await page.waitForSelector('.new-todo')
+
+  for (var i = 0; i< 15; i++){
+    await page.click('.new-todo')
+    await page.keyboard.type(`kup piwo ${i}`)
+    await page.keyboard.up('\n')
+  }
 
   await sleep(3000)
 
